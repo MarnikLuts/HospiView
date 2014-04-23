b6e9dcf7024d2494ad18d4a3dbe6b3b4b6aa39ff
frank <goyensfrank@gmail.com>
2014-04-23 16:43:42 +0200

    Now sending get requests instead of post requests

--- www/js/services.js
+++ www/js/services.js
@@ -17,40 +17,44 @@ angular.module('myApp.services', []).
         factory('hospiviewFactory', function($http, $rootScope, base_url) {
             return{
                 getHospiViewServerList: function() {
-                    $rootScope.requestCounter = $rootScope.requestCounter + 1;
-                    return $http.post("http://agenda.agendaview.be/cfcs/webservices/agendaview/hospiview_servers.cfc?method=GetHospiviewServerList");
+                    $rootScope.requestCounter++;
+                    return $http.get("http://agenda.agendaview.be/cfcs/webservices/agendaview/hospiview_servers.cfc?method=GetHospiviewServerList");
                 },
                 getAuthentication: function(username, password, server_url) {
-                    $rootScope.requestCounter = $rootScope.requestCounter + 1;
-                    return $http.post(server_url + base_url + "method=GetAuthentication&user_login=" + username + "&user_password=" + password + "&count=" + $rootScope.requestCounter);
+                    $rootScope.requestCounter++;
+                    return $http.get(server_url + base_url + "method=GetAuthentication&user_login=" + username + "&user_password=" + password + "&count=" + $rootScope.requestCounter);
                 },
                 getUnitAndDepList: function(uuid, server_url) {
-                    $rootScope.requestCounter = $rootScope.requestCounter + 1;
-                    return $http.post(server_url + base_url + "method=GetUnitAndDepList&UUID=" + uuid + "&count=" + $rootScope.requestCounter);
+                    $rootScope.requestCounter++;
+                    return $http.get(server_url + base_url + "method=GetUnitAndDepList&UUID=" + uuid + "&count=" + $rootScope.requestCounter);
                 },
                 getUnitDepGroups: function(uuid, server_url) {
-                    $rootScope.requestCounter = $rootScope.requestCounter + 1;
-                    return $http.post(server_url + base_url + "method=getUnitDepGroups&UUID=" + uuid + "&count=" + $rootScope.requestCounter);
+                    $rootScope.requestCounter++;
+                    return $http.get(server_url + base_url + "method=getUnitDepGroups&UUID=" + uuid + "&count=" + $rootScope.requestCounter);
                 },
                 getReservationsOnUnit: function(uuid, unit_id, dep_id, start_date, end_date, server_url) {
-                    $rootScope.requestCounter = $rootScope.requestCounter + 1;
-                    return $http.post(server_url + base_url + "method=GetReservationsOnUnit&UUID=" + uuid + "&unit_id=" + unit_id + "&dep_id=" + dep_id + "&start_date=" + start_date + "&end_date=" + end_date + "&count=" + $rootScope.requestCounter);
+                    $rootScope.requestCounter++;
+                    return $http.get(server_url + base_url + "method=GetReservationsOnUnit&UUID=" + uuid + "&unit_id=" + unit_id + "&dep_id=" + dep_id + "&start_date=" + start_date + "&end_date=" + end_date + "&count=" + $rootScope.requestCounter);
                 },
                 getReservationsOnPatient: function(uuid, pid_or_regno, patsearchvar, start_date, end_date, server_url) {
-                    $rootScope.requestCounter = $rootScope.requestCounter + 1;
-                    return $http.post(server_url + base_url + "method=GetReservationsOnPatient&UUID=" + uuid + "&pid_or_regno=" + pid_or_regno + "&patsearchvar=" + patsearchvar + "&start_date=" + start_date + "&end_date=" + end_date + "&count=" + $rootScope.requestCounter);
+                    $rootScope.requestCounter++;
+                    return $http.get(server_url + base_url + "method=GetReservationsOnPatient&UUID=" + uuid + "&pid_or_regno=" + pid_or_regno + "&patsearchvar=" + patsearchvar + "&start_date=" + start_date + "&end_date=" + end_date + "&count=" + $rootScope.requestCounter);
                 },
                 getPublicHolidays: function(Language_Id, year, month, server_url) {
-                    $rootScope.requestCounter = $rootScope.requestCounter + 1;
-                    return $http.post(server_url + base_url + "method=GetPublicHolidays&Language_Id=" + Language_Id + "&Year=" + year + "&Month=" + month + "&count=" + $rootScope.requestCounter);
+                    $rootScope.requestCounter++;
+                    return $http.get(server_url + base_url + "method=GetPublicHolidays&Language_Id=" + Language_Id + "&Year=" + year + "&Month=" + month + "&count=" + $rootScope.requestCounter);
                 },
                 getUnitAbsentDays: function(uuid, year, month, unit_id, server_url) {
-                    $rootScope.requestCounter = $rootScope.requestCounter + 1;
-                    return $http.post(server_url + base_url + "method=GetUnitAbsentDays&UUID=" + uuid + "&Year=" + year + "&Month=" + month + "&Unit_Id=" + unit_id + "&count=" + $rootScope.requestCounter);
+                    $rootScope.requestCounter++;
+                    return $http.get(server_url + base_url + "method=GetUnitAbsentDays&UUID=" + uuid + "&Year=" + year + "&Month=" + month + "&Unit_Id=" + unit_id + "&count=" + $rootScope.requestCounter);
                 },
                 getLanguageStrings: function(language_Id, listOfPidsSids, server_url) {
-                    $rootScope.requestCounter = $rootScope.requestCounter + 1;
-                    return $http.post(server_url + base_url + "method=GetLanguageStrings&Language_Id=" + language_Id + "&ListOfPidsSids=" + listOfPidsSids + "&count=" + $rootScope.requestCounter);
+                    $rootScope.requestCounter++;
+                    return $http.get(server_url + base_url + "method=GetLanguageStrings&Language_Id=" + language_Id + "&ListOfPidsSids=" + listOfPidsSids + "&count=" + $rootScope.requestCounter);
+                },
+                getLogin: function(USER_NAME, USER_REGNO, USER_EMAIL, USER_MOB, LanguageId, Update_NameEmailTel, server_url){
+                    $rootScope.requestCounter++;
+                    return $http.get(server_url + base_url + "method=GetLogin&USER_NAME=" + USER_NAME + "&USER_REGNO=" + USER_REGNO + "&USER_EMAIL=" + USER_EMAIL + "&USER_MOB=" + USER_MOB + "&LanguageId=" + LanguageId + "&Update_NameEmailTel=" + Update_NameEmailTel);
                 }
 
             };
