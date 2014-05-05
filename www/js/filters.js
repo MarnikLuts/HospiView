a96497b976cd192571e09fa3f4f4eecaf44372b9
Ceunen Stijn <stijn.ceunen@student.pxl.be>
2014-05-05 15:01:43 +0200

    Merge localOrigin/master
    Conflicts:
    	www/js/controllers.js

--- www/js/filters.js
+++ www/js/filters.js
@@ -26,20 +26,65 @@ angular.module('myApp.filters', []).
             };
         }).
         filter('orderProposals', function() {
-            return function(input, attribute) {
-                if (!angular.isObject(input))
-                    return input;
-
+            return function(proposals) {
+                var proposalPerDate = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []};
                 var array = [];
-                for (var objectKey in input) {
-                    array.push(input[objectKey]);
+                var lowestDate = new Date(2500,12,1);
+                
+                for (var proposal in proposals) {
+                    var dayNumber = new Date(proposals[proposal].the_date).getDay();
+                    var date = new Date(proposals[proposal].the_date);
+                    if(lowestDate > date)
+                        lowestDate = new Date(date);
+                    proposals[proposal].UTC = Date.UTC(date.getFullYear(),
+                            date.getMonth(),
+                            date.getDate(),
+                            proposals[proposal].time_from.substring(0, 2),
+                            proposals[proposal].time_from.substring(3, 5));
+                    proposalPerDate[dayNumber].push(proposals[proposal]);
                 }
 
-                array.sort(function(a, b) {
-                    a = parseInt(a[attribute]);
-                    b = parseInt(b[attribute]);
-                    return a - b;
-                });
+                var currentDayCounter = new Date().getDay();
+                for (var day in proposalPerDate) {
+                    if (proposalPerDate[currentDayCounter].length > 0) {
+                        proposalPerDate[currentDayCounter].sort(function(a, b) {
+                            return new Date(a.UTC) > new Date(b.UTC);
+                        });
+
+                        for (var proposal in proposalPerDate[currentDayCounter]) {
+                            array.push(proposalPerDate[currentDayCounter][proposal]);
+                        }
+                    }
+                    if (currentDayCounter === 6)
+                        currentDayCounter = 0;
+                    else
+                        currentDayCounter++;
+                }
+                console.log(array);
+
+                /*
+                 array.sort(function(a, b) {
+                 var current = new Date().getDay();
+                 var datea = new Date(a.the_date).getDay();
+                 var dateb = new Date(b.the_date).getDay();
+                 
+                 console.log("current: " + current);
+                 console.log("start a: " + datea);
+                 console.log("start b: " + dateb);
+                 
+                 if(datea >= current)
+                 datea = datea - current;
+                 else
+                 datea= datea + current;
+                 
+                 if(dateb >= current)
+                 dateb = dateb - current;
+                 else
+                 dateb = dateb + current;
+                 console.log("eind a: " + datea);
+                 console.log("eind b: " + dateb);
+                 return datea - dateb;
+                 });*/
                 return array;
             };
         });
